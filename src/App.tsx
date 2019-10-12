2459ca3f282685952eb480b3cfaddf805682bfc5
pieter <ppbraam@gmail.com>
2019-10-12 15:30:54 +0200

    inital commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
+            style={{ color: 'red' }}
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
