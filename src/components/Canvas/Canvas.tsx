56817619bb933f865b46570b2e6775e96c7aeca3
pieter <pieter.braam@create.nl>
2019-10-29 17:21:13 +0100

    progress

--- src/components/Canvas/Canvas.tsx
+++ src/components/Canvas/Canvas.tsx
@@ -1,20 +1,23 @@
-import React, { FC, ReactElement, useCallback } from 'react';
+import React, {
+  FC, ReactElement, useCallback, useState,
+} from 'react';
 import { useEffectOnce } from 'react-use';
 import * as THREE from 'three';
 import './Canvas.scss';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface CanvasType {
   onWheelTurn(speed: number): void;
 }
 
+interface MousePos {
+  x: number;
+  y: number;
+}
+
 const Canvas: FC<CanvasType> = ({ onWheelTurn }): ReactElement => {
   const ref = React.createRef<HTMLDivElement>();
-
-  const handleClick = useCallback(
-    () => onWheelTurn(5),
-    [onWheelTurn],
-  );
+  const [mouseStartPos, setMouseStartPos] = useState<MousePos>({ x: 0, y: 0 });
+  const [mouseAngle, setMouseAngle] = useState(0);
 
   useEffectOnce(() => {
     const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -38,9 +41,26 @@ const Canvas: FC<CanvasType> = ({ onWheelTurn }): ReactElement => {
     renderer.render(scene, camera);
   });
 
+  const handleMouseDown = (e: React.MouseEvent<HTMLElement>) => {
+    setMouseStartPos({ x: e.pageX, y: e.pageY });
+  };
+
+  const handleMouseMove = useCallback(
+    (e: React.MouseEvent<HTMLElement>) => {
+      const resultX = e.pageX - mouseStartPos.x;
+      const resultY = e.pageY - mouseStartPos.y;
+      const angle = (Math.atan2(resultY, resultX) * 180) / Math.PI;
+      const speed = angle - mouseAngle;
+
+      setMouseAngle(angle);
+      onWheelTurn(speed);
+    },
+    [onWheelTurn, mouseStartPos, setMouseAngle, mouseAngle],
+  );
+
   return (
     // eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions
-    <div className="canvas" ref={ref} onClick={handleClick} />
+    <div className="canvas" ref={ref} onMouseMove={handleMouseMove} onMouseDown={handleMouseDown} />
   );
 };
 
